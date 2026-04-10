c555695f774dd2d84e671c975b88a5d0b75201e5
felipe-mizidorio <felipemerenda1@gmail.com>
2026-04-10 10:46:39 +0100

    feat: initialize logging configuration in __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,  # DEBUG, INFO, WARNING, ERROR, CRITICAL
+    format="%(asctime)s - %(name)s:%(lineno)d - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[logging.StreamHandler()],
+)
